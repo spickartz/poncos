ec92f9f5c10db9f18e0ed42768458a6f09e19e03
Simon Pickartz <spickartz@eonerc.rwth-aachen.de>
2017-01-27 09:20:57 +0100

    implement generation of hosts-file for the cgroup controller

--- src/controller_cgroup.cpp
+++ src/controller_cgroup.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <cassert>
 #include <condition_variable>
+#include <fstream>
 #include <iostream>
 #include <numeric>
 #include <string>
@@ -88,26 +89,23 @@ controllerT::execute_config cgroup_controller::sort_config_by_hostname(const exe
 
 std::string cgroup_controller::generate_command(const jobT &job, size_t counter, const execute_config &config) const {
 	// index 0 == SLOT 0; index 1 == SLOT 1; index SLOTS == all slots on the same system are used
-	std::string host_lists[SLOTS + 1];
-	size_t hosts_per_slot[SLOTS + 1];
+	std::vector<std::string> host_lists[SLOTS + 1];
+	size_t hosts_per_slot[SLOTS + 1] = {0};
 	std::string commands[SLOTS + 1];
-
-	for (unsigned long &s : hosts_per_slot) s = 0;
-
 	execute_config sorted_config = sort_config_by_hostname(config);
 
 	for (size_t i = 0; i < config.size(); ++i) {
 		// both slots of a system used?
 		if (i + 1 < config.size() && config[i].first == config[i + 1].first) {
 			++i;
-			host_lists[SLOTS] += machines[config[i].first] + "," + machines[config[i].first];
+			host_lists[SLOTS].push_back(machines[config[i].first]);
+			host_lists[SLOTS].push_back(machines[config[i].first]);
 			hosts_per_slot[SLOTS] += 2;
 			continue;
 		}
-		host_lists[config[i].second] += machines[config[i].first] + ",";
+		host_lists[config[i].second].push_back(machines[config[i].first]);
 		++hosts_per_slot[config[i].second];
 	}
-
 	assert(job.req_cpus() ==
 		   std::accumulate(std::begin(hosts_per_slot), std::end(hosts_per_slot), size_t(0)) * SLOT_SIZE);
 
@@ -132,7 +130,7 @@ std::string cgroup_controller::generate_command(const jobT &job, size_t counter,
 		// remove last ','
 		command.pop_back();
 
-		command += job.command;
+		command += " " + job.command;
 		commands[slot] = command;
 	}
 
@@ -160,18 +158,33 @@ std::string cgroup_controller::generate_command(const jobT &job, size_t counter,
 		// remove last ','
 		command.pop_back();
 
-		command += job.command;
+		command += " " + job.command;
 		commands[SLOTS] = command;
 	}
+	// we should only use either of the slots or all of them
+	// TODO: do we want to handle unbalanced jobs?
+	assert(std::count(hosts_per_slot, hosts_per_slot + SLOTS + 1, 0) == SLOTS);
 
-	// TODO create hostfile
-	// filename should be something like cmd_name_from_id(counter)
-	// content:
+	// create hostfile
+	// filename: cmd_name_from_id(counter).hosts
+	// content: one line per allocated slot in accordance with the following scheme
+	// 	    <hostname>:job.nprocs/hosts_per_slot
+	std::string hosts_filename = cmd_name_from_id(counter) + ".hosts";
+	std::ofstream hosts_file(hosts_filename);
+	assert(hosts_file.is_open());
+	for (size_t i = 0; i <= SLOTS; ++i) {
+		if (hosts_per_slot[i] == 0) continue;
 
-	// TODO: add thread_per_procs to command string for the support of multi-threaded applications
-	//       need to change -np
+		for (const auto &host : host_lists[i]) {
+			hosts_file << host + ":";
+			hosts_file << job.nprocs / hosts_per_slot[i];
+			hosts_file << std::endl;
+		}
+	}
+	hosts_file.close();
+
 	std::string ret = "mpiexec ";
-	ret += "-f "; // TODO hostfilename
+	ret += "-f " + hosts_filename + " ";
 
 	// a colon must be added between slots
 	bool add_colon = false;
@@ -180,9 +193,9 @@ std::string cgroup_controller::generate_command(const jobT &job, size_t counter,
 
 		if (add_colon) ret += " : ";
 
-		ret += "-np " + std::to_string(hosts_per_slot[slot]) + " " + commands[slot];
+		ret += "-np " + std::to_string(job.nprocs) + " -genv OMP_NUM_THREADS " + std::to_string(job.threads_per_proc) +
+			   " " + commands[slot];
 		add_colon = true;
 	}
-
 	return ret;
 }
