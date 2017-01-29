407bc0e8ee739a8d1ad40826226f01aefba04ede
Jens Breitbart <jbreitbart@gmail.com>
2017-01-29 13:25:17 +0100

    Updated cgroup controller.

--- src/controller_cgroup.cpp
+++ src/controller_cgroup.cpp
@@ -29,6 +29,7 @@ cgroup_controller::cgroup_controller(const std::shared_ptr<fast::MQTT_communicat
 void cgroup_controller::init() {}
 void cgroup_controller::dismantle() {}
 
+// TODO should delete the mpi host files!
 cgroup_controller::~cgroup_controller() = default;
 
 void cgroup_controller::freeze(const size_t id) {
@@ -88,6 +89,9 @@ controllerT::execute_config cgroup_controller::sort_config_by_hostname(const exe
 }
 
 std::string cgroup_controller::generate_command(const jobT &job, size_t counter, const execute_config &config) const {
+
+	// TODO refactor, eg seperate file creation (shouln't that be part of controllerT)
+
 	// index 0 == SLOT 0; index 1 == SLOT 1; index SLOTS == all slots on the same system are used
 	std::vector<std::string> host_lists[SLOTS + 1];
 	size_t hosts_per_slot[SLOTS + 1] = {0};
@@ -97,13 +101,14 @@ std::string cgroup_controller::generate_command(const jobT &job, size_t counter,
 	for (size_t i = 0; i < config.size(); ++i) {
 		// both slots of a system used?
 		if (i + 1 < config.size() && config[i].first == config[i + 1].first) {
-			++i;
-			host_lists[SLOTS].push_back(machines[config[i].first]);
-			host_lists[SLOTS].push_back(machines[config[i].first]);
+			host_lists[SLOTS].emplace_back(machines[config[i].first]);
+			host_lists[SLOTS].emplace_back(machines[config[i].first]);
 			hosts_per_slot[SLOTS] += 2;
+
+			++i;
 			continue;
 		}
-		host_lists[config[i].second].push_back(machines[config[i].first]);
+		host_lists[config[i].second].emplace_back(machines[config[i].first]);
 		++hosts_per_slot[config[i].second];
 	}
 	assert(job.req_cpus() ==
@@ -112,7 +117,9 @@ std::string cgroup_controller::generate_command(const jobT &job, size_t counter,
 	for (size_t slot = 0; slot < SLOTS; ++slot) {
 		if (hosts_per_slot[slot] == 0) continue;
 
-		std::string command = " ./cgroup_wrapper.sh ";
+		std::string &command = commands[slot];
+
+		command = " ./cgroup_wrapper.sh ";
 		command += cmd_name_from_id(counter) + " ";
 
 		// cgroup CPUs and memory is set by the bash script
@@ -131,17 +138,17 @@ std::string cgroup_controller::generate_command(const jobT &job, size_t counter,
 		command.pop_back();
 
 		command += " " + job.command;
-		commands[slot] = command;
 	}
 
-	// some dedicated nodes
+	// some dedicated nodes, requires special cgroup config
 	if (hosts_per_slot[SLOTS] != 0) {
-		std::string command = " ./cgroup_wrapper.sh ";
+		std::string &command = commands[SLOTS];
+		command = " ./cgroup_wrapper.sh ";
 		command += cmd_name_from_id(counter) + " ";
 
 		// cgroup CPUs and memory is set by the bash script
-		for (size_t slot = 0; slot < SLOTS; ++slot) {
-			for (int i : co_configs[slot].cpus) {
+		for (const auto &co_config : co_configs) {
+			for (int i : co_config.cpus) {
 				command += std::to_string(i) + ",";
 			}
 		}
@@ -150,8 +157,8 @@ std::string cgroup_controller::generate_command(const jobT &job, size_t counter,
 
 		command += " ";
 
-		for (size_t slot = 0; slot < SLOTS; ++slot) {
-			for (int i : co_configs[slot].mems) {
+		for (const auto &co_config : co_configs) {
+			for (int i : co_config.mems) {
 				command += std::to_string(i) + ",";
 			}
 		}
@@ -159,25 +166,26 @@ std::string cgroup_controller::generate_command(const jobT &job, size_t counter,
 		command.pop_back();
 
 		command += " " + job.command;
-		commands[SLOTS] = command;
 	}
-	// we should only use either of the slots or all of them
-	// TODO: do we want to handle unbalanced jobs?
-	assert(std::count(hosts_per_slot, hosts_per_slot + SLOTS + 1, 0) == SLOTS);
 
 	// create hostfile
 	// filename: cmd_name_from_id(counter).hosts
 	// content: one line per allocated slot in accordance with the following scheme
-	// 	    <hostname>:job.nprocs/hosts_per_slot
+	//          <hostname>:job.nprocs/hosts_per_slot
 	std::string hosts_filename = cmd_name_from_id(counter) + ".hosts";
 	std::ofstream hosts_file(hosts_filename);
 	assert(hosts_file.is_open());
+
+	size_t total_number_of_slots = std::accumulate(std::cbegin(hosts_per_slot), std::cend(hosts_per_slot), 0);
+	size_t process_per_slot = total_number_of_slots / job.threads_per_proc;
+	assert(process_per_slot * job.threads_per_proc == total_number_of_slots * co_configs[0].cpus.size());
+
 	for (size_t i = 0; i <= SLOTS; ++i) {
 		if (hosts_per_slot[i] == 0) continue;
 
 		for (const auto &host : host_lists[i]) {
 			hosts_file << host + ":";
-			hosts_file << job.nprocs / hosts_per_slot[i];
+			hosts_file << process_per_slot;
 			hosts_file << std::endl;
 		}
 	}
@@ -193,7 +201,7 @@ std::string cgroup_controller::generate_command(const jobT &job, size_t counter,
 
 		if (add_colon) ret += " : ";
 
-		ret += " -np " + std::to_string(job.nprocs) + " " + commands[slot];
+		ret += " -np " + std::to_string(hosts_per_slot[slot]) + " " + commands[slot];
 		add_colon = true;
 	}
 	return ret;
