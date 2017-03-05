d5338f988c775176d6c4ce17bcc7378f0033b9ec
Simon Pickartz <spickartz@eonerc.rwth-aachen.de>
2017-03-05 17:39:53 +0100

    haswell adaptions

--- system_config/haswell_EP.cpp
+++ system_config/haswell_EP.cpp
@@ -4,12 +4,12 @@
 
 // This defines the slots used fot scheduling.
 // Currently there is a fixed number of two slots.
-const sched_configT co_configs_snb[SLOTS] = {
+const sched_configT co_configs[SLOTS] = {
 	// cpus used in slot 0
-	{{0, 1, 2, 3, 8, 9, 10, 11},
+	{{0, 1, 2, 3, 4, 5, 12, 13, 14, 15, 16, 17},
 	 // numa memory domains used in slot 0
 	 {0, 1}},
 	// cpus used in slot 1
-	{{4, 5, 6, 7, 12, 13, 14, 15},
+	{{6, 7, 8, 9, 10, 11, 18, 19, 20, 21, 22, 23},
 	 // numa memory domains used in slot 1
 	 {0, 1}}};
